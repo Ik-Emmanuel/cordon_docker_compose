915391c30a2036668d93b5cfb68a73ff99fd1500
Emmauel Ikechukwu <eik@pml.ac.uk>
2024-05-04 15:29:20 +0100

    basic form handling (to fix later prevent default for )

--- cordon_ui/app/(filters)/_components/SearchForm.tsx
+++ cordon_ui/app/(filters)/_components/SearchForm.tsx
@@ -34,33 +34,35 @@ const SearchForm = () => {
   return (
     <>
       <div className="flex w-[90%] md:w-full items-end justify-between gap-4 rounded-lg border border-neutral-600  backdrop-blur-lg sm:grid-cols-2 lg:grid-cols-2 p-6">
-        <div className="w-full">
-          <div className="relative">
-            {/* <span className="text-white font-semibold">
+        <form className="flex gap-2 w-full">
+          <div className="w-full">
+            <div className="relative">
+              {/* <span className="text-white font-semibold">
               Search For Datasets
             </span> */}
-            <Input
-              className="w-full"
-              value={searchWord}
-              placeholder="Search datasets using keywords"
-              onChange={handleInputChange}
-            />
+              <Input
+                className="w-full"
+                value={searchWord}
+                placeholder="Search datasets using keywords"
+                onChange={handleInputChange}
+              />
 
-            <SearchIcon className="text-black absolute right-2 bottom-2 text-[9px]" />
+              <SearchIcon className="text-black absolute right-2 bottom-2 text-[9px]" />
+            </div>
           </div>
-        </div>
 
-        <div className="w-[20%]">
-          <Link href={`/search?keyword=${searchWord}`}>
-            <Button
-              type="submit"
-              className="bg-cordon_sec-2 w-full text-white hover:bg-cordon_sec-1"
-            >
-              Search
-            </Button>
-          </Link>
-          {/* <SearchIcon /> */}
-        </div>
+          <div className="w-[20%]">
+            <Link href={`/search?keyword=${searchWord}`}>
+              <Button
+                type="submit"
+                className="bg-cordon_sec-2 w-full text-white hover:bg-cordon_sec-1"
+              >
+                Search
+              </Button>
+            </Link>
+            {/* <SearchIcon /> */}
+          </div>
+        </form>
       </div>
     </>
   );
